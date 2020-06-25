9b91a1048991b0a2c83f17a6e2e22deff6e3975f
Jon Grythe Stødle <jonstodle@outlook.com>
2020-06-25 21:38:13 +0200

    Improve visuals
    Change the background color, change the text color, make text uppercase and add text shadows

--- src/App.svelte
+++ src/App.svelte
@@ -21,7 +21,10 @@
       }
     }, {});
 
-  const startTime = dayjs().hour(params.hour).minute(params.minute).second(0);
+  const startTime = dayjs()
+    .hour(params.hour || dayjs().add(1, "h").hour())
+    .minute(params.minute || 0)
+    .second(0);
 
   let timeLeft = "--:--:--";
 
@@ -31,7 +34,12 @@
     const minutes = startTime.diff(now, "m");
     const seconds = startTime.diff(now, "s") - minutes * 60 - hours * 3600;
 
-    timeLeft = `${hours}:${minutes}:${seconds}`;
+    timeLeft = `${padLeft(hours)}:${padLeft(minutes)}:${padLeft(seconds)}`;
+  }
+
+  function padLeft(input) {
+    const asString = input.toString();
+    return `${"0".repeat(2 - asString.length)}${asString}`;
   }
 
   updateTimeLeft();
@@ -41,7 +49,8 @@
 <style>
   :global(html),
   :global(body) {
-    background-color: white;
+    background-color: #334;
+    color: #ddd;
     font-family: "JetBrains Mono", monospace;
     width: 100%;
     height: 100%;
@@ -51,15 +60,29 @@
     display: flex;
     justify-content: center;
     align-items: center;
-    font-size: 5vh;
+    font-size: 5vw;
   }
 
   main {
     text-align: center;
   }
+
+  h1,
+  h4 {
+    margin: 0;
+    text-shadow: #000 0px 20px;
+  }
+
+  h1 {
+    font-size: 3em;
+  }
+
+  h4 {
+    text-transform: uppercase;
+  }
 </style>
 
 <main>
-  <h2>{params.text || 'Starting in'}</h2>
+  <h4>{params.text || 'Starting in'}</h4>
   <h1>{timeLeft}</h1>
 </main>
